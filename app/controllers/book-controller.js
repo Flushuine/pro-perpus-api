d2f64d98be12e08b0e96dbb30258c60375503114
Muhammad Aqil <aqillatas33@gmail.com>
2021-09-13 16:12:26 +0700

    add update &  delete API

--- app/controllers/book-controller.js
+++ app/controllers/book-controller.js
@@ -3,8 +3,8 @@ import ModelBook from '../models/book-schema.js'
 //get book all data
 const getBooks = async(req, res) => {
     try {
-        const books = await ModelBook.find()
-        res.json(books)
+        const Books = await ModelBook.find()
+        res.json(Books)
     } catch(error) {
         res.status(500).json({
             message: error.message
@@ -15,8 +15,8 @@ const getBooks = async(req, res) => {
 //get a single book
 const getBookById = async(req, res) => {
     try {
-        const book = await ModelBook.findById(req.params.id)
-        res.json(book)
+        const Book = await ModelBook.findById(req.params.id)
+        res.json(Book)
     } catch(error) {
         res.status(404).json({
             message: error.message
@@ -26,9 +26,9 @@ const getBookById = async(req, res) => {
 
 //add a new book
 const saveBook = async(req, res) => {
-    const book = new ModelBook(req.body)
+    const Book = new ModelBook(req.body)
     try {
-        const savedBook = await book.save()
+        const savedBook = await Book.save()
         res.status(201).json(savedBook)
     } catch(error) {
         res.status(400).json({
@@ -37,4 +37,37 @@ const saveBook = async(req, res) => {
     }
 }
 
-export {getBooks, getBookById, saveBook}
+//update a book
+const updateBook = async(req, res) => {
+    const Book = await ModelBook.findById(req.params.id)
+    if(!Book) return res.status(404).json({
+        message: "Book with id=${id} not found!"
+    })
+    try {
+        const updatedBook = await ModelBook.updateOne({_id: req.params.id}, {$set: req.body})
+        res.status(200).json(updatedBook)
+        
+    } catch(error) {
+        res.status(400).json({
+            message: error.message
+        })
+    }
+}
+
+//delete a book
+const deleteBook = async(req, res) => {
+    const Book = await ModelBook.findById(req.params.id)
+    if(!Book) return res.status(404).json({
+        message: "Book with id=${id} not found!"
+    })
+    try {
+        const deletedBook = await ModelBook.deleteOne({_id: req.params.id})
+        res.status(200).json(deletedBook)
+    } catch(error) {
+        res.status(400).json({
+            message: error.message
+        })
+    }
+}
+
+export {getBooks, getBookById, saveBook, updateBook, deleteBook}
